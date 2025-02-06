8a9883ba57b912e47a69b62196b19bde706c438b
edlance1 <edlance@ntiertraining.com>
2025-02-06 05:58:21 -0500

    renamed run.py to wsgi.py and updated Dockerfile to reflect

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app import app
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #app.run(debug=True, host="0.0.0.0", port=5000)
 
     # "gunicorn", "--bind", "0.0.0.0:5000", --timeout 30 --workers 3 "run:app"
